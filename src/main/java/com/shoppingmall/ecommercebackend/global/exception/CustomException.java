dc8b19bb4fea025bcd6ceadd1cbb29b85f099bd6
wjdah1028 <wjdah1028@naver.com>
2026-07-13 16:00:50 +0900

    :wrench: Settings: exception 폴더 생성

---
+++ src/main/java/com/shoppingmall/ecommercebackend/global/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.shoppingmall.ecommercebackend.global.exception;
+
+import com.shoppingmall.ecommercebackend.global.exception.model.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final BaseErrorCode errorCode;
+
+    public CustomException(BaseErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
