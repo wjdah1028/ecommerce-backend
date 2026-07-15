b81967f9d6607cf774039db758eae74ce6f739a8
wjdah1028 <wjdah1028@naver.com>
2026-07-16 02:05:16 +0900

    :sparkles: Feat: 사용자 삭제 API 생성

--- src/main/java/com/shoppingmall/ecommercebackend/domain/user/repository/SocialAccountRepository.java
+++ src/main/java/com/shoppingmall/ecommercebackend/domain/user/repository/SocialAccountRepository.java
@@ -10,4 +10,7 @@ public interface SocialAccountRepository extends JpaRepository<SocialAccountEnti
 
     // 기존 회원인지 조회
     Optional<SocialAccountEntity> findByLoginProviderAndSocialUserId(String loginProvider, String socialUserId);
+
+    // UserEntity와 연관된 SocialAccountEntity 삭제
+    void deleteByUser(UserEntity user);
 }
