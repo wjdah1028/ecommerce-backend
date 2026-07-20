0211908346354ba7951e7aa5960979ab33cc70d9
wjdah1028 <wjdah1028@naver.com>
2026-07-20 21:07:41 +0900

    :sparkles: Feat: 브랜드 목록 조회, 수정, 삭제 API 생성

---
+++ src/main/java/com/shoppingmall/ecommercebackend/domain/brand/dto/request/BrandUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.shoppingmall.ecommercebackend.domain.brand.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@Setter
+@Schema(title = "브랜드 수정 요청 dto", description = "관리자가 브랜드를 수정할 때 서버에 요청 보내는 데이터")
+public class BrandUpdateRequest {
+
+    @Schema(description = "브랜드 이름", example = "나이키")
+    @NotBlank(message = "브랜드 이름은 필수 입력 값입니다.")
+    private String brandName;
+}
--- src/main/java/com/shoppingmall/ecommercebackend/domain/brand/service/BrandService.java
+++ src/main/java/com/shoppingmall/ecommercebackend/domain/brand/service/BrandService.java
@@ -1,7 +1,10 @@
 package com.shoppingmall.ecommercebackend.domain.brand.service;
 
 import com.shoppingmall.ecommercebackend.domain.brand.dto.request.BrandRegisterRequest;
+import com.shoppingmall.ecommercebackend.domain.brand.dto.request.BrandUpdateRequest;
 import com.shoppingmall.ecommercebackend.domain.brand.dto.response.BrandRegisterResponse;
+import com.shoppingmall.ecommercebackend.domain.brand.dto.response.BrandSearchResponse;
+import com.shoppingmall.ecommercebackend.domain.brand.dto.response.BrandUpdateResponse;
 import com.shoppingmall.ecommercebackend.domain.brand.entity.BrandEntity;
 import com.shoppingmall.ecommercebackend.domain.brand.exception.BrandErrorCode;
 import com.shoppingmall.ecommercebackend.domain.brand.repository.BrandRepository;
@@ -10,6 +13,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -46,5 +52,60 @@ public class BrandService {
                 .brandName(savedBrand.getBrandName())
                 .createdAt(savedBrand.getCreatedAt())
                 .build();
+    }
+
+    // 브랜드 목록 조회
+    public List<BrandSearchResponse> searchBrand() {
+
+        // 응답 세팅
+        List<BrandSearchResponse> list = new ArrayList<>();
+        for (BrandEntity brand : brandRepository.findAll()) {
+            list.add(BrandSearchResponse.builder()
+                    .brandId(brand.getBrandId())
+                    .brandName(brand.getBrandName())
+                    .build());
+        }
+
+        // 로그 출력
+        log.info("[BrandService] 브랜드 목록 조회 성공");
+
+        return list;
+    }
+
+    // 브랜드 수정
+    @Transactional
+    public BrandUpdateResponse updateBrand(Long brandId, BrandUpdateRequest request) {
+
+        // 브랜드가 존재하는지 조회
+        BrandEntity brand = brandRepository.findById(brandId)
+                .orElseThrow(() -> new CustomException(BrandErrorCode.BRAND_NOT_FOUND));
+
+        // 브랜드 수정
+        brand.updateBrand(request.getBrandName());
+
+        // 로그 출력
+        log.info("[BrandService] 브랜드 수정 성공: brandId={}", brand.getBrandId());
+
+        // 응답 세팅
+        return BrandUpdateResponse.builder()
+                .brandId(brand.getBrandId())
+                .brandName(brand.getBrandName())
+                .modifiedAt(brand.getModifiedAt())
+                .build();
+    }
+
+    // 브랜드 삭제
+    @Transactional
+    public void deleteBrand(Long brandId) {
+
+        // 브랜드가 존재하는지 조회
+        BrandEntity brand = brandRepository.findById(brandId)
+                .orElseThrow(() -> new CustomException(BrandErrorCode.BRAND_NOT_FOUND));
+
+        // 브랜드 삭제
+        brandRepository.delete(brand);
+
+        // 로그 출력
+        log.info("[BrandService] 브랜드 삭제 성공: brandId={}", brand.getBrandId());
     }
 }
