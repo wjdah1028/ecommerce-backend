65022a53729a000d0411c4a37c4a7d06d364dc7c
wjdah1028 <wjdah1028@naver.com>
2026-07-20 14:48:02 +0900

    :sparkles: Feat: 관리자 권한으로 리그 등록 API 생성

---
+++ src/main/java/com/shoppingmall/ecommercebackend/domain/league/repository/LeagueRepository.java
@@ -0,0 +1,10 @@
+package com.shoppingmall.ecommercebackend.domain.league.repository;
+
+import com.shoppingmall.ecommercebackend.domain.league.entity.LeagueEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeagueRepository extends JpaRepository<LeagueEntity, Long> {
+
+    // 등록된 리그 이름이 존재하는지 조회
+    boolean existsByLeagueName(String leagueName);
+}
