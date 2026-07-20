03bcfb200a0660333728acb051dd9de3752b7cfd
wjdah1028 <wjdah1028@naver.com>
2026-07-20 19:36:29 +0900

    :sparkles: Feat: 로그인 없이 리그 목록 조회하는 API

--- src/main/java/com/shoppingmall/ecommercebackend/domain/league/controller/LeagueController.java
+++ src/main/java/com/shoppingmall/ecommercebackend/domain/league/controller/LeagueController.java
@@ -2,8 +2,10 @@ package com.shoppingmall.ecommercebackend.domain.league.controller;
 
 import com.shoppingmall.ecommercebackend.domain.league.dto.request.LeagueRegisterRequest;
 import com.shoppingmall.ecommercebackend.domain.league.dto.response.LeagueRegisterResponse;
+import com.shoppingmall.ecommercebackend.domain.league.dto.response.LeagueSearchResponse;
 import com.shoppingmall.ecommercebackend.domain.league.service.LeagueService;
 import com.shoppingmall.ecommercebackend.global.common.BaseResponse;
+import com.shoppingmall.ecommercebackend.global.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -11,10 +13,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,7 +28,7 @@ public class LeagueController {
 
     // 리그 등록
     @Operation(summary = "리그 등록 API", description = "관리자가 리그 등록하는 API")
-    @PostMapping("/league")
+    @PostMapping("/leagues")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<BaseResponse<LeagueRegisterResponse>> leagueRegister(
             @Valid @RequestBody LeagueRegisterRequest request) {
@@ -36,5 +38,17 @@ public class LeagueController {
 
         // 응답 반환
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse.success(201, "리그 등록 성공", response));
+    }
+
+    // 리그 목록 조회
+    @Operation(summary = "리그 목록 조회 API", description = "사용자가 로그인 필요 없이 리그 목록 조회하는 API")
+    @GetMapping("/leagues")
+    public ResponseEntity<BaseResponse<List<LeagueSearchResponse>>> leagueSearch() {
+
+        // service 호출
+        List<LeagueSearchResponse> response = leagueService.searchLeague();
+
+        // 응답 반환
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.success(200, "리그 목록 조회 성공", response));
     }
 }
