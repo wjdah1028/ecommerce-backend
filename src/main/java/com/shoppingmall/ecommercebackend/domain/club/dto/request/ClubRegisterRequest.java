1ccf1d375289e29d2eefc917d53c8f7f26b048c8
wjdah1028 <wjdah1028@naver.com>
2026-07-24 03:34:11 +0900

    :sparkles: Feat: 관리자가 구단을 등록하는 API

---
+++ src/main/java/com/shoppingmall/ecommercebackend/domain/club/dto/request/ClubRegisterRequest.java
@@ -0,0 +1,23 @@
+package com.shoppingmall.ecommercebackend.domain.club.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@Setter
+@Schema(title = "구단 등록 요청 dto", description = "관리자가 구단을 등록할 때 서버에 요청 보내는 데이터")
+public class ClubRegisterRequest {
+
+    @Schema(description = "리그 고유번호", example = "1")
+    @NotNull(message = "리그 고유번호는 필수 입력 값입니다.")
+    private Long leagueId;
+
+    @Schema(description = "구단 이름", example = "맨체스터 유나이티드")
+    @NotBlank(message = "구단 이름은 필수 입력 값입니다.")
+    private String clubName;
+}
---
+++ src/main/java/com/shoppingmall/ecommercebackend/domain/club/service/ClubService.java
@@ -0,0 +1,63 @@
+package com.shoppingmall.ecommercebackend.domain.club.service;
+
+
+import com.shoppingmall.ecommercebackend.domain.club.dto.request.ClubRegisterRequest;
+import com.shoppingmall.ecommercebackend.domain.club.dto.response.ClubRegisterResponse;
+import com.shoppingmall.ecommercebackend.domain.club.entity.ClubEntity;
+import com.shoppingmall.ecommercebackend.domain.club.exception.ClubErrorCode;
+import com.shoppingmall.ecommercebackend.domain.club.repository.ClubRepository;
+import com.shoppingmall.ecommercebackend.domain.league.entity.LeagueEntity;
+import com.shoppingmall.ecommercebackend.domain.league.exception.LeagueErrorCode;
+import com.shoppingmall.ecommercebackend.domain.league.repository.LeagueRepository;
+import com.shoppingmall.ecommercebackend.global.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class ClubService {
+
+    private final ClubRepository clubRepository;
+    private final LeagueRepository leagueRepository;
+
+    // 구단 등록
+    @Transactional
+    public ClubRegisterResponse registerClub(ClubRegisterRequest request, Long leagueId) {
+
+        // 리그가 존재하는지 조회
+        LeagueEntity league = leagueRepository.findById(leagueId)
+                .orElseThrow(() -> new CustomException(LeagueErrorCode.LEAGUE_NOT_FOUND));
+
+        // 구단이 등록됐는지 조회
+        if (clubRepository.existsByClubName(request.getClubName())) {
+            log.warn("[ClubService] 등록된 구단입니다");
+            throw new CustomException(ClubErrorCode.CLUB_DUPLICATE);
+        }
+
+        // 구단 객체 생성
+        ClubEntity club =
+                ClubEntity.builder()
+                        .league(league)
+                        .clubName(request.getClubName())
+                        .build();
+
+        // DB 저장
+        ClubEntity savedClub = clubRepository.save(club);
+
+        // 로그 출력
+        log.info("[ClubService] 구단 등록 성공: clubName={}", request.getClubName());
+
+        // 응답 세팅
+        return ClubRegisterResponse.builder()
+                .clubId(savedClub.getClubId())
+                .leagueId(savedClub.getLeague().getLeagueId())
+                .clubName(savedClub.getClubName())
+                .leagueName(savedClub.getLeague().getLeagueName())
+                .createdAt(savedClub.getCreatedAt())
+                .build();
+    }
+}
