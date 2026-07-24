40255e96872438cd7e92d0c7f232045e8b00f444
wjdah1028 <wjdah1028@naver.com>
2026-07-25 03:56:35 +0900

    :sparkles: Feat: 리그별 구단 조회 API

--- src/main/java/com/shoppingmall/ecommercebackend/domain/club/repository/ClubRepository.java
+++ src/main/java/com/shoppingmall/ecommercebackend/domain/club/repository/ClubRepository.java
@@ -1,10 +1,16 @@
 package com.shoppingmall.ecommercebackend.domain.club.repository;
 
 import com.shoppingmall.ecommercebackend.domain.club.entity.ClubEntity;
+import com.shoppingmall.ecommercebackend.domain.league.entity.LeagueEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ClubRepository extends JpaRepository<ClubEntity, Long> {
 
     // 등록된 구단 이름이 존재하는지 조회
     boolean existsByClubName(String clubName);
+
+    // 리그 별 구단 조회
+    List<ClubEntity> findAllByLeague(LeagueEntity league);
 }
