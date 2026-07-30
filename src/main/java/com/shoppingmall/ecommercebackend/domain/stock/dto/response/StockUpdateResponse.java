14587d02e648cccbb24b116b2938ce20417112c3
wjdah1028 <wjdah1028@naver.com>
2026-07-30 22:38:30 +0900

    :sparkles: Feat: 재고 수정 API 생성

---
+++ src/main/java/com/shoppingmall/ecommercebackend/domain/stock/dto/response/StockUpdateResponse.java
@@ -0,0 +1,31 @@
+package com.shoppingmall.ecommercebackend.domain.stock.dto.response;
+
+import com.shoppingmall.ecommercebackend.domain.stock.entity.Size;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(title = "재고 수정 응답 dto", description = "판매자가 재고를 수정할때 서버가 반환하는 데이터")
+public class StockUpdateResponse {
+
+    @Schema(description = "유니폼 고유번호", example = "1")
+    private Long uniformId;
+
+    @Schema(description = "유니폼 이름", example = "맨체스터 유나이티드 26/27 어센틱 홈 저지")
+    private String uniformName;
+
+    @Schema(description = "사이즈", example = "M")
+    private Size size;
+
+    @Schema(description = "수량", example = "10")
+    private Integer stockQuantity;
+
+    @Schema(description = "재고 수정 시간", example = "2026-07-017T04:00:00")
+    private LocalDateTime modifiedAt;
+}
